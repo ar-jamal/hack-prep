cb2b4f3e55e9afb86d4ed0ee74cff5b5c07e4dbf
Salman Karim <salmankarim.khan42@gmail.com>
2022-11-12 13:48:24 +0500

    jamal changes

--- src/Screens/DashboardScreens/quizForm.js
+++ src/Screens/DashboardScreens/quizForm.js
@@ -19,25 +19,29 @@ export default function QuizForm() {
   const [open, setOpen] = useState(false);
 
   const inputChangeHandler = (key, val) => {
-    console.log(val);
+    // console.log(val);
     inputValues[key] = val;
     setInputValues({ ...inputValues, ansOptions: ansOptions });
     console.log(inputValues);
   };
+  const onAnsInputHandler = (i, val) => {
+    const copyOptions = [...ansOptions]
+    copyOptions[i] = val 
+    setAnsOptions([...copyOptions]);
+  };
 
   const optionsInput = (options) => {
-    console.log(options);
+    // console.log(options);
     let items = [];
     for (let i = 0; i < options; i++) {
       items.push(
-        <Grid item xs={4}>
+        <Grid key={i} item xs={4}>
           <CusInput
-            key={i}
+            // key={i}
+            
             label={`Option-${i + 1}`}
-            onChange={(e) =>
-              setAnsOptions([...ansOptions.push(items[i].e.target.value)])
-            }
-            // value={ansOptions[i]}
+            onChange={(e) => onAnsInputHandler(i, e.target.value)}
+            value={ansOptions[i]}
           />
         </Grid>
       );
@@ -47,89 +51,88 @@ export default function QuizForm() {
 
   const addQuestion = () => {
     setFilledQues([...filledQues.push(inputValues)]);
-    console.log(filledQues)
+    console.log(filledQues);
   };
-
+  console.log(ansOptions)
   return (
-    <div sx={{ width: { sm: `calc(100% - 420px)` } }}>
+    <div sx={{ width: { sm: `calc(100% - 420px)` }, display: "flex" }}>
       <h2 style={{ marginBlock: "4%", fontSize: 28, textAlign: "center" }}>
         QUIZ FORM
       </h2>
-      <div className="Body">
-        <Grid container columnSpacing={3}>
-          <Grid item xs={8}>
-            <CusInput
-              required={true}
-              label="Question"
-              // placeholder= "create questions"
-              onChange={(e) => inputChangeHandler("Question", e.target.value)}
-            />
-          </Grid>
-          <Grid item xs={4}>
-            <CusInput
-              label="Correct Answer *"
-              onChange={(e) =>
-                inputChangeHandler("CorrectAnswer", e.target.value)
-              }
-            />
-          </Grid>
-          <Grid item xs={12} sx={{ marginTop: "30" }}>
-            <Button
-              onClick={() => setOptions(options + 1)}
-              style={{
-                minWidth: "15%",
-                alignSelf: "start",
-                marginBlock: 20,
-                fontSize: 18,
-              }}
-            >
-              Add Ans Options
-            </Button>
-          </Grid>
-          {!!options && optionsInput(options)}
-          <Grid item xs={12}>
-            <Button
-              variant="contained"
-              onClick={() => addQuestion()}
-              style={{
-                minWidth: "15%",
-                alignSelf: "start",
-                marginBlock: 20,
-                fontSize: 18,
-              }}
-            >
-              Add more Questions
-            </Button>
-          </Grid>
-          <Grid item xs={12}>
-            <CusInput
-              label="Duration *"
-              onChange={(e) => inputChangeHandler("Duration", e.target.value)}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <CusInput
-              label="Total Marks"
-              onChange={(e) => inputChangeHandler("TotalMarks", e.target.value)}
-            />
-          </Grid>
+      {/* <div className="Body"> */}
+      <Grid container columnSpacing={3}>
+        <Grid item xs={8}>
+          <CusInput
+            required={true}
+            label="Question"
+            // placeholder= "create questions"
+            onChange={(e) => inputChangeHandler("Question", e.target.value)}
+          />
         </Grid>
-        <Button
-          variant="contained"
-          style={{
-            width: "15%",
-            alignSelf: "end",
-            marginBlock: 20,
-            fontSize: 18,
-          }}
-          // onClick={onSubmitHandler}
-        >
-          SUBMIT
-        </Button>
-        <Grid container spacing={4}>
-          {filledQues &&
-            filledQues.length > 0 
-            ? filledQues.map((e, i) => (
+        <Grid item xs={4}>
+          <CusInput
+            label="Correct Answer *"
+            onChange={(e) =>
+              inputChangeHandler("CorrectAnswer", e.target.value)
+            }
+          />
+        </Grid>
+        <Grid item xs={12} sx={{ marginTop: "30" }}>
+          <Button
+            onClick={() => setOptions(options + 1)}
+            style={{
+              minWidth: "15%",
+              alignSelf: "start",
+              marginBlock: 20,
+              fontSize: 18,
+            }}
+          >
+            Add Ans Options
+          </Button>
+        </Grid>
+        {!!options && optionsInput(options)}
+        <Grid item xs={12}>
+          <Button
+            variant="contained"
+            onClick={() => addQuestion()}
+            style={{
+              minWidth: "15%",
+              alignSelf: "start",
+              marginBlock: 20,
+              fontSize: 18,
+            }}
+          >
+            Add more Questions
+          </Button>
+        </Grid>
+        <Grid item xs={12}>
+          <CusInput
+            label="Duration *"
+            onChange={(e) => inputChangeHandler("Duration", e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <CusInput
+            label="Total Marks"
+            onChange={(e) => inputChangeHandler("TotalMarks", e.target.value)}
+          />
+        </Grid>
+      </Grid>
+      <Button
+        variant="contained"
+        style={{
+          width: "15%",
+          alignSelf: "end",
+          marginBlock: 20,
+          fontSize: 18,
+        }}
+        // onClick={onSubmitHandler}
+      >
+        SUBMIT
+      </Button>
+      <Grid container spacing={4}>
+        {filledQues && filledQues.length > 0
+          ? filledQues.map((e, i) => (
               <>
                 <Grid item xs={12}>
                   <Typography>{filledQues[i].question}</Typography>
@@ -137,17 +140,18 @@ export default function QuizForm() {
                 <Grid item xs={12}>
                   <Typography>{filledQues[i].correctAns}</Typography>
                 </Grid>
-                {ansOptions &&
-                  ansOptions > 0
+                {ansOptions && ansOptions > 0
                   ? ansOptions.map((x, j) => (
-                    <Grid item xs={4}>
-                      <Typography>{filledQues[i].ansOptions[j]}</Typography>
-                    </Grid>
-                  )) : null}
+                      <Grid item xs={4}>
+                        <Typography>{filledQues[i].ansOptions[j]}</Typography>
+                      </Grid>
+                    ))
+                  : null}
               </>
-            )) : null}
-        </Grid>
-      </div>
+            ))
+          : null}
+      </Grid>
+      {/* </div> */}
     </div>
   );
 }
