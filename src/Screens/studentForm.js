1f4da2ebfca81f90306a0504de11f7971116ffc0
Salman Karim <salmankarim.khan42@gmail.com>
2022-11-18 18:25:04 +0500

    validation completed

--- src/Screens/studentForm.js
+++ src/Screens/studentForm.js
@@ -1,5 +1,5 @@
 import "../App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button, Typography } from "@mui/material";
 import CusInput from "../utils/components/MaterialUi/cusInput";
 import CusSelect from "../utils/components/MaterialUi/cusSelect";
@@ -9,9 +9,10 @@ import TextField from "@mui/material/TextField";
 import { set } from "firebase/database";
 import { sendData } from "../Config/firebaseMethods";
 import { Password } from "@mui/icons-material";
+import { async } from "@firebase/util";
 
 export default function StudentForm() {
-  const [formData, setFormData] = useState([])
+  const [formData, setFormData] = useState([]);
   const [filledForm, setFilledForm] = useState({});
   const [course, setCourse] = useState("");
   const [sec, setSec] = useState("");
@@ -19,67 +20,82 @@ export default function StudentForm() {
   const [age, setAge] = useState("");
   const [ageDisabled, setAgeDisabled] = useState(false);
   const [open, setOpen] = useState(false);
-  const [count, setCount] = useState(0)
-  const [fig, setFig] = useState("00")
-  const [rollNo, setRollNo] = useState("")
-  const [alertTitle, setAlertTitle] = useState("")
-  const [alertMessage, setAlertMessage] = useState("")
-  const [nodeId, setNodeId] = useState("")
+  const [count, setCount] = useState(0);
+  const [fig, setFig] = useState("00");
+  const [rollNo, setRollNo] = useState("");
+  const [alertTitle, setAlertTitle] = useState("");
+  const [alertMessage, setAlertMessage] = useState("");
+  const [nodeId, setNodeId] = useState("");
 
   const isReqField =
-    filledForm.firstName &&
-    filledForm.constact &&
-    filledForm.email &&
-    filledForm.password &&
-    filledForm.course &&
-    filledForm.sec &&
-    filledForm.cnic &&
-    filledForm.fatherName &&
-    filledForm.fatherContact &&
-    filledForm.emergencyContact;
+    !filledForm.firstName ||
+    !filledForm.constact ||
+    !filledForm.email ||
+    !filledForm.password ||
+    !filledForm.course ||
+    !filledForm.sec ||
+    !filledForm.cnic ||
+    !filledForm.fatherName ||
+    !filledForm.fatherContact ||
+    !filledForm.emergencyContact;
 
   const countHandler = () => {
-    setCount((formData.length + 1).toString())
+    setCount((formData.length + 1).toString());
     if (count.length > 1) {
-      setFig("0")
+      setFig("0");
     } else if (count.length > 2) {
-      setFig("")
+      setFig("");
     }
-    console.log(count)
-    let figCount = fig.concat(count)
-    return figCount
-  }
+    // console.log(count);
+    let figCount = fig.concat(count);
+    return figCount;
+  };
 
   const generateStudentId = () => {
     const figCount = countHandler();
-    const stuNamePart = filledForm.firstName?.slice(0, 3) ?? ''
-    const fathNamePart = filledForm.fatherName?.slice(0, 3) ?? ''
-    const cnicPart = filledForm.cnic?.slice(-3) ?? ''
+    const stuNamePart = filledForm.firstName?.slice(0, 3) ?? "";
+    const fathNamePart = filledForm.fatherName?.slice(0, 3) ?? "";
+    const cnicPart = filledForm.cnic?.slice(-3) ?? "";
 
-    return stuNamePart + fathNamePart + filledForm.course + filledForm.sec.toUpperCase() + cnicPart + figCount
-  }
+    return (
+      stuNamePart +
+      fathNamePart +
+      filledForm.course +
+      filledForm.sec.toUpperCase() +
+      cnicPart +
+      figCount
+    );
+  };
 
   const inputChangeHandler = (key, val) => {
     filledForm[key] = val;
     setFilledForm({ ...filledForm });
   };
-  const dateChangeHandler = (key, val) => {
-    const dateString = val.toString();
+  async function dateChangeHandler(key, val) {
+    const dateString = val;
     const formattedDate = new Date(dateString).toString().slice(4, 15);
     const curDate = new Date().toDateString().slice(4, 15);
     const miliSec = Date.parse(curDate) - Date.parse(`${formattedDate}`);
     const year = Math.floor(miliSec / (1000 * 60 * 60 * 24 * 365)) || "";
     setAge(year);
-    if (age <= 0) {
-      setAlertTitle("incorrect date")
-      setAlertMessage("Plz select back date in Date of birth field")
-      setOpen(true)
-      return
-    } else {
-      filledForm[key] = val;
-      setFilledForm({ ...filledForm });
-    }
+    console.log(age);
   }
+  const ageUpdated = () => {
+    if (!!age && age <= 0) {
+      setAlertTitle("incorrect date");
+      setAlertMessage("Plz select back date in Date of birth field");
+      setOpen(true);
+      return;
+    } else {
+      filledForm["age"] = age;
+      setFilledForm({ ...filledForm });
+      console.log(filledForm.age);
+    }
+  };
+  useEffect(() => {
+    ageUpdated();
+  }, [age]);
+
   const onCourChangeHandler = (key, val) => {
     setCourse(val);
     filledForm[key] = val;
@@ -93,48 +109,48 @@ export default function StudentForm() {
   const onAlertClose = () => {
     setOpen(false);
     setAgeDisabled(false);
-    setAlertTitle("")
-    setAlertMessage("")
+    // setAlertTitle("");
+    // setAlertMessage("");
   };
   const ageDisabledHandler = () => {
     setAgeDisabled(true);
-    setAlertTitle("Date of Birth required only")
-    setAlertMessage("Age will be calculated on it")
+    setAlertTitle("Date of Birth required only");
+    setAlertMessage("Age will be calculated on it");
     setOpen(true);
   };
 
   const onSubmitHandler = () => {
-    console.log(filledForm.fatherName)
-    console.log(formData.length);
-    if (age <= 0) {
-      setOpen(true)
-      return
-    } else if (!!isReqField) {
-      setAlertTitle("Required field error")
-      setAlertMessage("Plz must fill all required fields")
-      setOpen(true)
-      return
+    console.log(isReqField);
+    // console.log(formData.length);
+    if (!!age && age <= 0) {
+      setOpen(true);
+      return;
+    } else if (!isReqField) {
+      setAlertTitle("Required field error");
+      setAlertMessage("Plz must fill all req fields");
+      setOpen(!!alertTitle && alertMessage ? true : false);
+      return;
+    } else {
+      filledForm.rollNumber = generateStudentId();
+      filledForm.registrationDate = new Date().toISOString().slice(0, 10);
+      filledForm.isFeeSubmitted = false;
+      filledForm.isApproved = false;
+      filledForm.isActive = false;
+      formData.push(filledForm);
+      console.log(formData);
+      sendData(formData, "Student", nodeId)
+        .then((success) => {
+          setAlertTitle(success.message);
+          setAlertMessage("");
+          setOpen(!!alertTitle ? true : false);
+          // setNodeId(success.nodeId)
+          console.log(success.obj);
+        })
+        .then((err) => {
+          setAlertTitle("error message");
+        });
     }
-    filledForm.rollNumber = generateStudentId()
-    filledForm.registrationDate = new Date().toISOString().slice(0, 10)
-    filledForm.isFeeSubmitted = false
-    filledForm.isApproved = false
-    filledForm.isActive = false
-    formData.push(filledForm)
-    console.log(formData)
-    sendData(formData, "Student", nodeId)
-      .then((success) => {
-        setAlertTitle(success.message);
-        setAlertMessage("")
-        setOpen(true)
-        // setNodeId(success.nodeId)
-        console.log(success.obj)
-      })
-      .then((err) => {
-        setAlertTitle(err)
-      })
-
-  }
+  };
 
   return (
     <div className="Layout">
@@ -221,17 +237,13 @@ export default function StudentForm() {
             <CusInput
               required={true}
               label="Father Name"
-              onChange={(e) =>
-                inputChangeHandler("fatherName", e.target.value)
-              }
+              onChange={(e) => inputChangeHandler("fatherName", e.target.value)}
             />
           </Grid>
           <Grid item xs={6}>
             <CusInput
               label="Father CNIC"
-              onChange={(e) =>
-                inputChangeHandler("fatherCNIC", e.target.value)
-              }
+              onChange={(e) => inputChangeHandler("fatherCNIC", e.target.value)}
             />
           </Grid>
           <Grid item xs={6}>
@@ -274,8 +286,8 @@ export default function StudentForm() {
               value={age}
               open={open}
               onClose={onAlertClose}
-              alertTitle={alertTitle? alertTitle : ""}
-              alertMessage={alertMessage? alertMessage : ""}
+              alertTitle={alertTitle ? alertTitle : ""}
+              alertMessage={alertMessage ? alertMessage : ""}
             />
           </Grid>
         </Grid>
